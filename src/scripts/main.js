f29f4c7cf89f7614cd7a27ab0bbe61e73712bf64
Mykhailo Doloh <dologmihajlo31@gmail.com>
2025-08-15 21:41:06 +0300

    add task

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,26 @@
 'use strict';
 
-// write code here
+// eslint-disable-next-line no-shadow
+function sortList(list) {
+  const items = Array.from(list.children);
+
+  items.sort((a, b) => Number(b.dataset.salary) - Number(a.dataset.salary));
+
+  items.forEach((item) => list.appendChild(item));
+}
+
+// eslint-disable-next-line no-shadow
+function getEmployees(list) {
+  return Array.from(list.children).map((item) => ({
+    name: item.dataset.name,
+    position: item.dataset.position,
+    salary: Number(item.dataset.salary),
+    age: Number(item.dataset.age),
+  }));
+}
+
+// Виклик функцій
+const list = document.querySelector('[data-list]');
+
+sortList(list);
+getEmployees(list);
